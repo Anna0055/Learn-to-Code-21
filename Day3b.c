93f42113aae18a1bcbca935de2f7b6bf3cbd4920
Anna0055 <83949802+Anna0055@users.noreply.github.com>
2021-05-13 22:30:10 +0530

    Create Day3b.c

---
+++ Day3b.c
@@ -0,0 +1,17 @@
+// Write a C Program to convert Fahrenheit to Celsius
+
+
+#include <stdio.h>
+
+
+int main() {
+
+    float Fahrenheit,Celsius;
+    
+    scanf("%f" ,&Fahrenheit);
+    
+    Celsius = (Fahrenheit-32)* 5/9;
+        
+    printf("%.2f " ,Celsius) ;   
+    return 0;
+}
