ae9ac1d00d05e7613f0d498b0685fe7ff7d16f0e
Anna0055 <83949802+Anna0055@users.noreply.github.com>
2021-05-14 14:06:25 +0530

    Create Day4a.c

---
+++ Day4a.c
@@ -0,0 +1,24 @@
+//Write a C Program to verify if a given year is a leap year or no
+
+#include <stdio.h>
+int main()
+{
+int year;
+
+scanf("%d",&year); 
+if(year%4 == 0) 
+{
+if(year%100 == 0) 
+{ 
+if (year%400 == 0)    
+printf("Yes");
+else 
+printf("No"); 
+}
+else 
+printf("Yes"); 
+} 
+else 
+printf("No"); 
+return 0; 
+}
