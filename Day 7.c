e1148912d55c630158c8abc620009df3eafaa7df
Anna0055 <83949802+Anna0055@users.noreply.github.com>
2021-06-09 21:52:39 +0530

    Create Day 7.c

---
+++ Day 7.c
@@ -0,0 +1,43 @@
+//Write a C Program to display each day of the week using switch case
+
+#include <stdio.h>
+ 
+int main() {
+
+    
+    int day;
+    
+    scanf("%d",&day);
+     
+    
+    switch(day)
+        
+    {
+        case 1:
+            printf("Sunday\n");
+            break;
+        case 2: 
+            printf("Monday\n");
+            break;
+        case 3: 
+            printf("Tuesday\n");
+            break;
+        case 4: 
+            printf("Wednesday\n");
+            break;
+        case 5: 
+            printf("Thursday\n");
+            break;
+        case 6: 
+            printf("Friday\n");
+            break;
+        case 7: 
+            printf("Saturday\n");
+            break;
+        default:
+            printf("Error\n");
+
+    }
+   
+    return 0;
+}
