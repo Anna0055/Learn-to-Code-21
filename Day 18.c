c169b4b737c8edd9ca0c546a6339bb8579bbc7aa
Anna0055 <83949802+Anna0055@users.noreply.github.com>
2021-06-09 22:16:53 +0530

    Create Day 18.c

---
+++ Day 18.c
@@ -0,0 +1,36 @@
+//Write a C program to find the frequency of odd and even numbers in the 1D array.
+#include <stdio.h>
+int main()
+{
+    
+    int a[1000],i,n,even=0,odd=0;
+ 
+        
+    scanf("%d", &n);
+ 
+        
+        for (i = 0; i < n; i++) 
+        {
+            scanf("%d", &a[i]);
+        }
+          
+        for (i = 0; i < n; i++)
+        {
+            
+            if ((a[i]% 2) == 0)
+                     
+            ++even;
+                     
+            else
+                
+            ++odd;
+        }
+ 
+ 
+        printf("%d \n", even);
+        printf("%d\n", odd);
+          
+          
+        return 0;  
+ 
+    }
